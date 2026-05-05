2a02d8e063980e33aaf7d1c5529cc67ce9018e67
slandarer <1410814635@qq.com>
2026-05-05 23:54:47 +0800

    add & push by Matlab

---
+++ demo1_SCM.m
@@ -0,0 +1,17 @@
+% demo1
+surf(peaks,'EdgeColor','w','EdgeAlpha',.3)
+% 使用slanCM的彩虹配色
+% Use rainbow colormap
+colormap(slanCM('rainbow'))
+
+
+% 修饰一下
+% Decorate the axes
+ax=gca;
+ax.Projection='perspective';
+ax.LineWidth=1.2;
+ax.XMinorTick='on';
+ax.YMinorTick='on';
+ax.ZMinorTick='on';
+ax.GridLineStyle=':';
+view(-37,42) 
---
+++ demo2_SCM.m
@@ -0,0 +1,19 @@
+% demo2
+XData=rand(15,15);
+XData=XData+XData.';
+H=fspecial('average',3);
+XData=imfilter(XData,H,'replicate');
+
+imagesc(XData)
+% 使用slanCM的100号配色
+% Use colormap No. 100 from slanCM
+colormap(slanCM(100))
+hold on
+
+ax=gca;
+ax.DataAspectRatio=[1,1,1];
+% ax.LineWidth=1.2;
+% ax.XMinorTick='on';
+% ax.YMinorTick='on';
+% ax.ZMinorTick='on';
+% ax.GridLineStyle=':';
